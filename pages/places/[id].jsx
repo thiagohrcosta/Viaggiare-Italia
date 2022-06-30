845edbc84b500022c5e1b52ebae94ed51add4153
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-06-29 21:56:01 -0300

    Adding initial places page

---
+++ pages/places/[id].jsx
@@ -0,0 +1,14 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Place(props) {
+  return (
+    <>
+      <main>
+        <title>{props?.name} | Viaggiare Italia</title>
+      </main>
+      <Box>
+        <Text color={"#fff"}></Text>
+      </Box>
+    </>
+  )
+}
